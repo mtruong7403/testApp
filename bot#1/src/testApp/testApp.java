7e4b1d8c198af4a4cf825d8d9bf928becd76a8c3
mtruong7403 <truongtmichael99@gmail.com>
2021-05-30 03:45:19 -0700

    fix

--- bot#1/src/testApp/testApp.java
+++ bot#1/src/testApp/testApp.java
@@ -1,8 +1,23 @@
 package testApp;
 
+
+
+import javax.security.auth.login.LoginException;
+
 import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.OnlineStatus;
+
 
 public class testApp{
    public static JDA jda;
-    
-}
+   public static JDABuilder builder;
+
+    public static void main(String[] args) throws LoginException{
+        jda = JDABuilder.createDefault("ODQ4NDk2MDQ1NDAyMDk1NjU2.YLNdhQ.sGTmG5IRqXB_Ng0ewNVXnB_QDFI").build();
+     
+        
+
+        
+    }
+} 
